8ffcdd54e627d11e8dba352d135f96a9a9c2dfc6
emailhy <emailhy@gmail.com>
2019-11-07 09:47:59 -0600

    Updates

---
+++ precache-manifest.4fe9a7b57ef005d361d1131ca6cf41dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2c4995592f4fe3a3a42c766426c3570",
+    "url": "/tsguru/index.html"
+  },
+  {
+    "revision": "c82ae36ad0143b1f8ceb",
+    "url": "/tsguru/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "d9eb2bea3b367fc8d241",
+    "url": "/tsguru/static/js/2.69b10efc.chunk.js"
+  },
+  {
+    "revision": "c82ae36ad0143b1f8ceb",
+    "url": "/tsguru/static/js/main.d36a93f5.chunk.js"
+  },
+  {
+    "revision": "a30b8d463577d80287e0",
+    "url": "/tsguru/static/js/runtime-main.e65fb337.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/tsguru/static/media/logo.25bf045c.svg"
+  }
+]);
